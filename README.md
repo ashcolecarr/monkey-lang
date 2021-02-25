c511973edc257b90983703a13fb5dd16c57078d8
ashcolecarr <ashcolecarr@gmail.com>
2021-02-25 10:04:30 -0600

    Clearing old files.

--- README.md
+++
@@ -1,3 +0,0 @@
-# Monkey Interpreter and Compiler
-
-A Rust implentation of the Monkey interpreter and compiler, as described in *Writing an Interpreter in Go* and *Writing a Compiler in Go* by Thorsten Ball.
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,3 @@
-use std::env;
-use monkey_lang::repl;
-
 fn main() {
-    let args: Vec<String> = env::args().collect();
-
-    println!("This is the Monkey programming language!");
-    println!("Type in commands:");
-
-    if args.len() == 1 {
-        repl::start();
-    }
-    //else if args.len() == 2 {
-    //    run_file(&mut vm, &args[1]);
-    //}
-    else {
-        println!("Usage: ");
-    }
+    println!("Hello, world!");
 }
