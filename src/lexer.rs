333ea00f9c5ee0592b1dc02927eebbcb944f1a52
ashcolecarr <ashcolecarr@gmail.com>
2021-02-26 12:07:00 -0600

    Adding token lexing.

---
+++ src/lexer.rs
@@ -0,0 +1,275 @@
+use super::token::{Token, TokenType};
+
+pub struct Lexer {
+    pub input: Vec<char>,
+    pub position: usize,
+    pub read_position: usize,
+    pub ch: char,
+}
+
+impl Lexer {
+    pub fn new(input: &str) -> Self {
+        let mut lexer = Self {
+            input: input.chars().collect(),
+            position: 0,
+            read_position: 0,
+            ch: '\0',
+        };
+
+        lexer.read_char();
+
+        lexer
+    }
+
+    pub fn next_token(&mut self) -> Token {
+        let mut literal = String::new();
+
+        self.skip_whitespace();
+
+        let token_type = match self.ch {
+            '=' => {
+                if self.peek_char() == '=' {
+                    let ch = self.ch;
+                    self.read_char();
+                    literal.push(ch);
+                    literal.push(self.ch);
+                    self.read_char();
+
+                    TokenType::Eq
+                } else {
+                    TokenType::Assign
+                }
+            },
+            '+' => TokenType::Plus,
+            '-' => TokenType::Minus,
+            '!' => {
+                if self.peek_char() == '=' {
+                    let ch = self.ch;
+                    self.read_char();
+                    literal.push(ch);
+                    literal.push(self.ch);
+                    self.read_char();
+
+                    TokenType::NotEq
+                } else {
+                    TokenType::Bang
+                }
+            },
+            '/' => TokenType::Slash,
+            '*' => TokenType::Asterisk,
+            '<' => TokenType::Lt,
+            '>' => TokenType::Gt,
+            ';' => TokenType::Semicolon,
+            '(' => TokenType::LParen,
+            ')' => TokenType::RParen,
+            ',' => TokenType::Comma,
+            '{' => TokenType::LBrace,
+            '}' => TokenType::RBrace,
+            '\0' => TokenType::Eof,
+            _ => {
+                if self.is_letter() {
+                    literal = self.read_identifier();
+
+                    Token::lookup_ident(literal.as_str())
+                } else if self.is_digit() {
+                    literal = self.read_number();
+
+                    TokenType::Int
+                } else {
+                    TokenType::Illegal
+                }
+            },
+        };
+
+        // Exit early here since the identifier's characters are already consumed.
+        if !literal.is_empty() {
+            return Token::new(token_type, literal);
+        }
+
+        let token = Token::new(token_type, self.ch.to_string());
+        self.read_char();
+
+        token
+    }
+
+    fn read_char(&mut self) {
+        if self.read_position >= self.input.len() {
+            self.ch = '\0';
+        } else {
+            self.ch = self.input[self.read_position];
+        }
+
+        self.position = self.read_position;
+        self.read_position += 1;
+    }
+
+    fn read_identifier(&mut self) -> String {
+        let position = self.position;
+
+        while self.is_letter() {
+            self.read_char();
+        }
+
+        self.input[position..self.position].iter().collect()
+    }
+
+    fn read_number(&mut self) -> String {
+        let position = self.position;
+
+        while self.is_digit() {
+            self.read_char();
+        }
+
+        self.input[position..self.position].iter().collect()
+    }
+
+    fn is_letter(&self) -> bool {
+        match self.ch {
+            'a'..='z' | 'A'..='Z' | '_' => true,
+            _ => false,
+        }
+    }
+
+    fn is_digit(&self) -> bool {
+        match self.ch {
+            '0'..='9' => true,
+            _ => false,
+        }
+    }
+
+    fn skip_whitespace(&mut self) {
+        loop {
+            match self.ch {
+                ' ' | '\t' | '\n' | '\r' => self.read_char(),
+                _ => break,
+            }
+        };
+    }
+
+    fn peek_char(&self) -> char {
+        if self.read_position >= self.input.len() {
+            '\0'
+        } else {
+            self.input[self.read_position]
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_next_token() {
+        let input = r#"let five = 5;
+let ten = 10;
+
+let add = fn(x, y) {
+  x + y;
+};
+
+let result = add(five, ten);
+!-/*5;
+5 < 10 > 5;
+
+if (5 < 10) {
+    return true;
+} else {
+    return false;
+}
+
+10 == 10;
+10 != 9;"#;
+
+        struct TokenTest<'a> {
+            expected_type: TokenType,
+            expected_literal: &'a str,
+        };
+
+        let token_tests = vec![
+            TokenTest { expected_type: TokenType::Let, expected_literal: "let" },
+            TokenTest { expected_type: TokenType::Ident, expected_literal: "five" },
+            TokenTest { expected_type: TokenType::Assign, expected_literal: "=" },
+            TokenTest { expected_type: TokenType::Int, expected_literal: "5" },
+            TokenTest { expected_type: TokenType::Semicolon, expected_literal: ";" },
+            TokenTest { expected_type: TokenType::Let, expected_literal: "let" },
+            TokenTest { expected_type: TokenType::Ident, expected_literal: "ten" },
+            TokenTest { expected_type: TokenType::Assign, expected_literal: "=" },
+            TokenTest { expected_type: TokenType::Int, expected_literal: "10" },
+            TokenTest { expected_type: TokenType::Semicolon, expected_literal: ";" },
+            TokenTest { expected_type: TokenType::Let, expected_literal: "let" },
+            TokenTest { expected_type: TokenType::Ident, expected_literal: "add" },
+            TokenTest { expected_type: TokenType::Assign, expected_literal: "=" },
+            TokenTest { expected_type: TokenType::Function, expected_literal: "fn" },
+            TokenTest { expected_type: TokenType::LParen, expected_literal: "(" },
+            TokenTest { expected_type: TokenType::Ident, expected_literal: "x" },
+            TokenTest { expected_type: TokenType::Comma, expected_literal: "," },
+            TokenTest { expected_type: TokenType::Ident, expected_literal: "y" },
+            TokenTest { expected_type: TokenType::RParen, expected_literal: ")" },
+            TokenTest { expected_type: TokenType::LBrace, expected_literal: "{" },
+            TokenTest { expected_type: TokenType::Ident, expected_literal: "x" },
+            TokenTest { expected_type: TokenType::Plus, expected_literal: "+" },
+            TokenTest { expected_type: TokenType::Ident, expected_literal: "y" },
+            TokenTest { expected_type: TokenType::Semicolon, expected_literal: ";" },
+            TokenTest { expected_type: TokenType::RBrace, expected_literal: "}" },
+            TokenTest { expected_type: TokenType::Semicolon, expected_literal: ";" },
+            TokenTest { expected_type: TokenType::Let, expected_literal: "let" },
+            TokenTest { expected_type: TokenType::Ident, expected_literal: "result" },
+            TokenTest { expected_type: TokenType::Assign, expected_literal: "=" },
+            TokenTest { expected_type: TokenType::Ident, expected_literal: "add" },
+            TokenTest { expected_type: TokenType::LParen, expected_literal: "(" },
+            TokenTest { expected_type: TokenType::Ident, expected_literal: "five" },
+            TokenTest { expected_type: TokenType::Comma, expected_literal: "," },
+            TokenTest { expected_type: TokenType::Ident, expected_literal: "ten" },
+            TokenTest { expected_type: TokenType::RParen, expected_literal: ")" },
+            TokenTest { expected_type: TokenType::Semicolon, expected_literal: ";" },
+            TokenTest { expected_type: TokenType::Bang, expected_literal: "!" },
+            TokenTest { expected_type: TokenType::Minus, expected_literal: "-" },
+            TokenTest { expected_type: TokenType::Slash, expected_literal: "/" },
+            TokenTest { expected_type: TokenType::Asterisk, expected_literal: "*" },
+            TokenTest { expected_type: TokenType::Int, expected_literal: "5" },
+            TokenTest { expected_type: TokenType::Semicolon, expected_literal: ";" },
+            TokenTest { expected_type: TokenType::Int, expected_literal: "5" },
+            TokenTest { expected_type: TokenType::Lt, expected_literal: "<" },
+            TokenTest { expected_type: TokenType::Int, expected_literal: "10" },
+            TokenTest { expected_type: TokenType::Gt, expected_literal: ">" },
+            TokenTest { expected_type: TokenType::Int, expected_literal: "5" },
+            TokenTest { expected_type: TokenType::Semicolon, expected_literal: ";" },
+            TokenTest { expected_type: TokenType::If, expected_literal: "if" },
+            TokenTest { expected_type: TokenType::LParen, expected_literal: "(" },
+            TokenTest { expected_type: TokenType::Int, expected_literal: "5" },
+            TokenTest { expected_type: TokenType::Lt, expected_literal: "<" },
+            TokenTest { expected_type: TokenType::Int, expected_literal: "10" },
+            TokenTest { expected_type: TokenType::RParen, expected_literal: ")" },
+            TokenTest { expected_type: TokenType::LBrace, expected_literal: "{" },
+            TokenTest { expected_type: TokenType::Return, expected_literal: "return" },
+            TokenTest { expected_type: TokenType::True, expected_literal: "true" },
+            TokenTest { expected_type: TokenType::Semicolon, expected_literal: ";" },
+            TokenTest { expected_type: TokenType::RBrace, expected_literal: "}" },
+            TokenTest { expected_type: TokenType::Else, expected_literal: "else" },
+            TokenTest { expected_type: TokenType::LBrace, expected_literal: "{" },
+            TokenTest { expected_type: TokenType::Return, expected_literal: "return" },
+            TokenTest { expected_type: TokenType::False, expected_literal: "false" },
+            TokenTest { expected_type: TokenType::Semicolon, expected_literal: ";" },
+            TokenTest { expected_type: TokenType::RBrace, expected_literal: "}" },
+            TokenTest { expected_type: TokenType::Int, expected_literal: "10" },
+            TokenTest { expected_type: TokenType::Eq, expected_literal: "==" },
+            TokenTest { expected_type: TokenType::Int, expected_literal: "10" },
+            TokenTest { expected_type: TokenType::Semicolon, expected_literal: ";" },
+            TokenTest { expected_type: TokenType::Int, expected_literal: "10" },
+            TokenTest { expected_type: TokenType::NotEq, expected_literal: "!=" },
+            TokenTest { expected_type: TokenType::Int, expected_literal: "9" },
+            TokenTest { expected_type: TokenType::Semicolon, expected_literal: ";" },
+            TokenTest { expected_type: TokenType::Eof, expected_literal: "\0" },
+        ];
+
+        let mut lexer = Lexer::new(input);
+
+        for token_test in token_tests {
+            let token = lexer.next_token();
+
+            assert_eq!(token.token_type, token_test.expected_type);
+            assert_eq!(token.literal, token_test.expected_literal);
+        }
+    }
+}
---
+++ src/token.rs
@@ -0,0 +1,70 @@
+use std::fmt::{Display, Formatter, Result};
+
+#[derive(Debug, PartialEq)]
+pub enum TokenType {
+    Illegal,
+    Eof,
+
+    // Identifiers and literals
+    Ident,
+    Int,
+
+    // Operators
+    Assign,
+    Plus,
+    Minus,
+    Bang,
+    Asterisk,
+    Slash,
+    Lt,
+    Gt,
+    Eq,
+    NotEq,
+
+    // Delimiters
+    Comma,
+    Semicolon,
+    LParen,
+    RParen,
+    LBrace,
+    RBrace,
+
+    // Keywords
+    Function,
+    Let,
+    True,
+    False,
+    If,
+    Else,
+    Return,
+}
+
+impl Display for TokenType {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+pub struct Token {
+    pub token_type: TokenType,
+    pub literal: String,
+}
+
+impl Token {
+    pub fn new(token_type: TokenType, literal: String) -> Self {
+        Self { token_type, literal }
+    }
+
+    pub fn lookup_ident(ident: &str) -> TokenType {
+        match ident {
+            "fn" => TokenType::Function,
+            "let" => TokenType::Let,
+            "true" => TokenType::True,
+            "false" => TokenType::False,
+            "if" => TokenType::If,
+            "else" => TokenType::Else,
+            "return" => TokenType::Return,
+            _ => TokenType::Ident,
+        }
+    }
+}
